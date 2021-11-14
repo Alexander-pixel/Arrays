bf96ab74af0e510321ade6b14180a192f92e15e0
Alexander <58190800+Alexander-pixel@users.noreply.github.com>
2021-11-14 21:28:44 +0200

    Update 3-unique.js

--- Exercises/3-unique.js
+++ Exercises/3-unique.js
@@ -3,6 +3,13 @@
 // Create and return a new array without duplicate elements
 // Don't modify initial array
 
-const unique = (array) => [];
+const unique = (array) => {
+  const result = [];
+  for (let i = 0; i < array.length; i++) {
+    if (!result.includes(array[i]))
+      result.push(array[i]);
+  }
+  return result;
+};
 
 module.exports = { unique };
